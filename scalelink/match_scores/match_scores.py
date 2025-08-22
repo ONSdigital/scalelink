0b917327ccd3c404f3e62cc2c45dcaee142eb90d
Gavin Thomson <19821607+grthomson@users.noreply.github.com>
2025-08-22 13:22:18 +0000

    fix: amend "import *" and "== True" for ruff pre-commit checks

--- scalelink/match_scores/match_scores.py
+++ scalelink/match_scores/match_scores.py
@@ -1,6 +1,6 @@
 """Match score functions.
 
-A series of functions for calculating Scalelink match weights and Scalelink 
+A series of functions for calculating Scalelink match weights and Scalelink
 match scores, given existing Scalelink deltas as defined in the 2017 paper
 by Goldstein et al.
 
@@ -11,9 +11,9 @@ Methods:
 
   assign_weights:
     A method to calculate Scalelink match weights for each ID column pair,
-    given as inputs a set of Scalelink deltas (agreement state values for ID 
-    pairs), and a scaled vector x* (agreement state weights by matching 
-    variable). Acts on the outputs of calculate_deltas, scale_x_star, and 
+    given as inputs a set of Scalelink deltas (agreement state values for ID
+    pairs), and a scaled vector x* (agreement state weights by matching
+    variable). Acts on the outputs of calculate_deltas, scale_x_star, and
     get_input_variables.
 
   get_match_scores:
@@ -23,213 +23,181 @@ Methods:
      - assign_match_score
 """
 
-import re
-
-from pyspark import *
 from pyspark.sql import functions as F
 
+
 def assign_match_score(df_with_weights):
-  """
-  Takes a dataframe made by Cartesian join of two datasets to be linked, with
-      Scalelink weights assigned and calculates match score for each row.
-  
-  Args:
-      df_with_weights (Spark DataFrame):
-          A dataframe consisting of a row ID for the first dataset that was
-          joined, a row ID for the second dataset that was joined and a column
-          for each Scalelink weight for each linkage variable.
-  
-  Returns:
-      df_with_match_score (Spark DataFrame):
-          A dataframe consisting of df_with_weights with an additional column
-          called match_score which contains the sum of the weights, row-wise.
-  """
-  weight_cols = [col for col in df_with_weights.columns
-                    if col.endswith('_weight')]
-  
-  # Make match score
-  df_with_match_score = df_with_weights\
-  .withColumn(
-    'match_score',
-    F.lit(0)
-  )
-  
-  # Fill match score
-  for col in weight_cols:
-    df_with_match_score = df_with_match_score\
-    .withColumn(
-    'match_score',
-    F.col(col) + F.col('match_score')
-  )
-  
-  return df_with_match_score
+    """
+    Takes a dataframe made by Cartesian join of two datasets to be linked, with
+        Scalelink weights assigned and calculates match score for each row.
 
-def assign_weights(df_with_deltas, df1_id, df2_id, cutpoints, x_star_scaled,
-                   spark):
-  """
-  Takes a dataframe made by Cartesian join of two datasets to be linked, with
-      Scalelink deltas (an indicator matrix calculated for the agreement states
-      of the linkage variables) calculated. Also takes the pre-calculated
-      Scalelink weights derived from that dataframe. Returns the ID columns
-      from that dataframe, plus the weights assigned to each linkage variable
-      for each row (i.e., for each ID column pair).
-  
-  Args:
-      df_with_deltas (Spark DataFrame):
-          A dataframe produced by the calculate_deltas() function.
-      df1_id (str):
-          The name of the column in df_with_deltas that contains the row ID
-          for the first dataset that was joined.
-      df2_id (str):
-          The name of the column in df_with_deltas that contains the row ID
-          for the second dataset that was joined.
-      cutpoints (dict of str: float):
-          A dictionary with keys consisting of the linkage variable names and
-          values consisting of the string comparison cutpoints for those
-          variables.
-      x_star_scaled (dict of str: float):
-          A dictionary with keys consisting of linkage variable names and
-          states and the values consisting of the scaled weights calculated for
-          those variables and states.
-      spark (PySpark SparkSession):
-          Name of the Spark session being used.
-  
-  Dependencies:
-      re
+    Args:
+        df_with_weights (Spark DataFrame):
+            A dataframe consisting of a row ID for the first dataset that was
+            joined, a row ID for the second dataset that was joined and a column
+            for each Scalelink weight for each linkage variable.
 
-  Returns:
-      df_with_weights (Spark DataFrame):
-          A dataframe consisting of df1_id and df2_id from df_with_deltas with
-          weight columns (named after the linkage variables, suffixed with
-          '_weight') containing appropriate weights from x_star_scaled for
-          each linkage variable and row.
-  """
-  # Define variables
-  df_with_weights = df_with_deltas
-  linkage_cols = list(cutpoints.keys())
-  
-  # Make then fill weight columns
-  for col in linkage_cols:
-    
-    df_with_weights = df_with_weights\
-    .withColumn(
-      col + '_weight',
-      F.lit(None)
+    Returns:
+        df_with_match_score (Spark DataFrame):
+            A dataframe consisting of df_with_weights with an additional column
+            called match_score which contains the sum of the weights, row-wise.
+    """
+    weight_cols = [col for col in df_with_weights.columns if col.endswith("_weight")]
+
+    # Make match score
+    df_with_match_score = df_with_weights.withColumn("match_score", F.lit(0))
+
+    # Fill match score
+    for col in weight_cols:
+        df_with_match_score = df_with_match_score.withColumn(
+            "match_score", F.col(col) + F.col("match_score")
+        )
+
+    return df_with_match_score
+
+
+def assign_weights(df_with_deltas, df1_id, df2_id, cutpoints, x_star_scaled, spark):
+    """
+    Takes a dataframe made by Cartesian join of two datasets to be linked, with
+        Scalelink deltas (an indicator matrix calculated for the agreement states
+        of the linkage variables) calculated. Also takes the pre-calculated
+        Scalelink weights derived from that dataframe. Returns the ID columns
+        from that dataframe, plus the weights assigned to each linkage variable
+        for each row (i.e., for each ID column pair).
+
+    Args:
+        df_with_deltas (Spark DataFrame):
+            A dataframe produced by the calculate_deltas() function.
+        df1_id (str):
+            The name of the column in df_with_deltas that contains the row ID
+            for the first dataset that was joined.
+        df2_id (str):
+            The name of the column in df_with_deltas that contains the row ID
+            for the second dataset that was joined.
+        cutpoints (dict of str: float):
+            A dictionary with keys consisting of the linkage variable names and
+            values consisting of the string comparison cutpoints for those
+            variables.
+        x_star_scaled (dict of str: float):
+            A dictionary with keys consisting of linkage variable names and
+            states and the values consisting of the scaled weights calculated for
+            those variables and states.
+        spark (PySpark SparkSession):
+            Name of the Spark session being used.
+
+    Dependencies:
+        re
+
+    Returns:
+        df_with_weights (Spark DataFrame):
+            A dataframe consisting of df1_id and df2_id from df_with_deltas with
+            weight columns (named after the linkage variables, suffixed with
+            '_weight') containing appropriate weights from x_star_scaled for
+            each linkage variable and row.
+    """
+    # Define variables
+    df_with_weights = df_with_deltas
+    linkage_cols = list(cutpoints.keys())
+
+    # Make then fill weight columns
+    for col in linkage_cols:
+
+        df_with_weights = df_with_weights.withColumn(col + "_weight", F.lit(None))
+
+        for key in [x for x in x_star_scaled.keys() if x.startswith(col)]:
+            if "_disagree" in key:
+                df_with_weights = df_with_weights.withColumn(
+                    col + "_weight",
+                    F.when(
+                        F.col("di_" + col + "_1"), F.lit(x_star_scaled[key])
+                    ).otherwise(F.col(col + "_weight")),
+                )
+            elif "_partially_agree_" in key:
+                df_with_weights = df_with_weights.withColumn(
+                    col + "_weight",
+                    F.when(
+                        F.col(
+                            "di_"
+                            + col
+                            + "_"
+                            + str(int(key.replace(col + "_partially_agree_", "")) + 1)
+                        ),
+                        F.lit(x_star_scaled[key]),
+                    ).otherwise(F.col(col + "_weight")),
+                )
+            elif ("_agree" in key) & (cutpoints[col] is None):
+                df_with_weights = df_with_weights.withColumn(
+                    col + "_weight",
+                    F.when(
+                        F.col("di_" + col + "_2"), F.lit(x_star_scaled[key])
+                    ).otherwise(F.col(col + "_weight")),
+                )
+            else:
+                df_with_weights = df_with_weights.withColumn(
+                    col + "_weight",
+                    F.when(
+                        F.col("di_" + col + "_" + str(len(cutpoints[col]) + 1)),
+                        F.lit(x_star_scaled[key]),
+                    ).otherwise(F.col(col + "_weight")),
+                )
+
+    # Drop unnecessary columns
+    df_with_weights = df_with_weights.select(
+        [df1_id, df2_id]
+        + [col for col in df_with_weights.columns if col.endswith("_weight")]
     )
-    
-    for key in [x for x in x_star_scaled.keys() if x.startswith(col)]:
-      if '_disagree' in key:
-        df_with_weights = df_with_weights\
-        .withColumn(
-          col + '_weight',
-          F.when(
-            F.col('di_' + col + '_1') == True,
-            F.lit(x_star_scaled[key])
-          )\
-          .otherwise(
-            F.col(col + '_weight')
-          )
-        )
-      elif '_partially_agree_' in key:
-        df_with_weights = df_with_weights\
-        .withColumn(
-          col + '_weight',
-          F.when(
-            F.col(
-              'di_' + col + '_' +
-              str(int(key.replace(col + '_partially_agree_', '')) + 1)
-            ) == True,
-            F.lit(x_star_scaled[key])
-          )\
-          .otherwise(
-            F.col(col + '_weight')
-          )
-        )
-      elif ('_agree' in key) & (cutpoints[col] == None):
-        df_with_weights = df_with_weights\
-        .withColumn(
-          col + '_weight',
-          F.when(
-            F.col('di_' + col + '_2') == True,
-            F.lit(x_star_scaled[key])
-          )\
-          .otherwise(
-            F.col(col + '_weight')
-          )
-        )
-      else:
-        df_with_weights = df_with_weights\
-        .withColumn(
-          col + '_weight',
-          F.when(
-            F.col('di_' + col + '_' + str(len(cutpoints[col]) + 1)) == True,
-            F.lit(x_star_scaled[key])
-          )\
-          .otherwise(
-            F.col(col + '_weight')
-          )
-        )
-      
-  # Drop unnecessary columns
-  df_with_weights = df_with_weights\
-  .select(
-    [df1_id, df2_id] +
-    [col for col in df_with_weights.columns if col.endswith('_weight')]
-  )
-  
-  return df_with_weights
 
-def get_match_scores(df_deltas, x_star_scaled_labelled, input_variables,
-                     spark):
-  """
-  Takes a dataframe containing a Cartesian join of the two dataframes to be
-      compared, with deltas calculated. Also takes the scaled, labelled weights
-      (x*) for this dataframe and the input variables (stored in a dictionary).
-      From this, returns the IDs from each row of the incoming dataframe with
-      weights and matchscores appropriately assigned.
-  
-  Args:
-      df_deltas (Spark DataFrame):
-          A dataframe consisting of df_cartesian_join with additional Boolean
-          columns containing Scalelink deltas (i.e, an indicator matrix) for
-          the linkage variables.
-      x_star_scaled_labelled (dict of str: float):
-          A dictionary with keys consisting of linkage variable names and
-          states and the values consisting of the scaled weights calculated for
-          those variables and states.
-      input_variables (dict of str, misc):
-          A dictionary containing the other input variables required for
-          the scaling algorithm, including ID column names and string comparator
-          cut-points. Dictionary keys are the names of the input variables and
-          dictionary values are the values of the variables themselves. 
-          This may be produced by the utils function get_input_variables().
-      spark (PySpark SparkSession):
-          Name of the Spark session being used.
-  
-  Dependencies:
-      re
-      match_scores.assign_weights
-      match_scores.assign_match_score
-      
-  Returns:
-      df_weights_match_scores (Spark DataFrame):
-          A dataframe consisting of df1_id and df2_id from df_with_deltas with
-          weight columns (named after the linkage variables, suffixed with
-          '_weight') containing appropriate weights from x_star_scaled for
-          each linkage variable and row, plus a column called match_score which
-          contains the sum of the weights, row-wise.
-  """
-  df_weights = msf.assign_weights(
-    df_with_deltas = df_deltas,
-    df1_id = input_variables['df1_id'],
-    df2_id = input_variables['df2_id'],
-    cutpoints = input_variables['cutpoints'],
-    x_star_scaled = x_star_scaled_labelled,
-    spark = spark
-  )
-  
-  df_weights_match_scores = msf.assign_match_score(
-    df_with_weights = df_weights
-  )
-  
-  return df_weights_match_scores
+    return df_with_weights
+
+
+def get_match_scores(df_deltas, x_star_scaled_labelled, input_variables, spark):
+    """
+    Takes a dataframe containing a Cartesian join of the two dataframes to be
+        compared, with deltas calculated. Also takes the scaled, labelled weights
+        (x*) for this dataframe and the input variables (stored in a dictionary).
+        From this, returns the IDs from each row of the incoming dataframe with
+        weights and matchscores appropriately assigned.
+
+    Args:
+        df_deltas (Spark DataFrame):
+            A dataframe consisting of df_cartesian_join with additional Boolean
+            columns containing Scalelink deltas (i.e, an indicator matrix) for
+            the linkage variables.
+        x_star_scaled_labelled (dict of str: float):
+            A dictionary with keys consisting of linkage variable names and
+            states and the values consisting of the scaled weights calculated for
+            those variables and states.
+        input_variables (dict of str, misc):
+            A dictionary containing the other input variables required for
+            the scaling algorithm, including ID column names and string comparator
+            cut-points. Dictionary keys are the names of the input variables and
+            dictionary values are the values of the variables themselves.
+            This may be produced by the utils function get_input_variables().
+        spark (PySpark SparkSession):
+            Name of the Spark session being used.
+
+    Dependencies:
+        re
+        match_scores.assign_weights
+        match_scores.assign_match_score
+
+    Returns:
+        df_weights_match_scores (Spark DataFrame):
+            A dataframe consisting of df1_id and df2_id from df_with_deltas with
+            weight columns (named after the linkage variables, suffixed with
+            '_weight') containing appropriate weights from x_star_scaled for
+            each linkage variable and row, plus a column called match_score which
+            contains the sum of the weights, row-wise.
+    """
+    df_weights = assign_weights(
+        df_with_deltas=df_deltas,
+        df1_id=input_variables["df1_id"],
+        df2_id=input_variables["df2_id"],
+        cutpoints=input_variables["cutpoints"],
+        x_star_scaled=x_star_scaled_labelled,
+        spark=spark,
+    )
+
+    df_weights_match_scores = assign_match_score(df_with_weights=df_weights)
+
+    return df_weights_match_scores
--- tests/match_scores/test_match_scores.py
+++ tests/match_scores/test_match_scores.py
@@ -13,113 +13,144 @@ import pytest
 
 from scalelink.match_scores import match_scores as msf
 
+
 def test_assign_match_score(spark):
-  """
-  Tests that assign_match_score() gives the correct output when provided with
-  suitable inputs.
-  
-  Dependencies:
-    chispa as ch
-  """
-  # Arrange
-  test_input = spark.createDataFrame(
-    [
-      ('1-01', '2-01', 75.0,  91.0),
-      ('1-02', '2-02',  0.0,  33.0),
-      ('1-03', '2-03',  0.0,  0.0)
-    ],
-    [
-      'id_df1', 'id_df2', 'sex_weight', 'forename_weight'
-    ]
-  )
-  
-  expected_output = spark.createDataFrame(
-    [
-      ('1-01', '2-01', 75.0,  91.0, 166.0),
-      ('1-02', '2-02',  0.0,  33.0,  33.0),
-      ('1-03', '2-03',  0.0,  0.0,    0.0)
-    ],
-    [
-      'id_df1', 'id_df2', 'sex_weight', 'forename_weight', 'match_score'
-    ]
-  )
-  
-  # Act
-  test_output = msf.assign_match_score(
-    df_with_weights = test_input
-  )
-  
-  # Assert
-  ch.assert_df_equality(
-    df1 = test_output,
-    df2 = expected_output,
-    ignore_row_order = True
-  )
+    """
+    Tests that assign_match_score() gives the correct output when provided with
+    suitable inputs.
+
+    Dependencies:
+      chispa as ch
+    """
+    # Arrange
+    test_input = spark.createDataFrame(
+        [
+            ("1-01", "2-01", 75.0, 91.0),
+            ("1-02", "2-02", 0.0, 33.0),
+            ("1-03", "2-03", 0.0, 0.0),
+        ],
+        ["id_df1", "id_df2", "sex_weight", "forename_weight"],
+    )
+
+    expected_output = spark.createDataFrame(
+        [
+            ("1-01", "2-01", 75.0, 91.0, 166.0),
+            ("1-02", "2-02", 0.0, 33.0, 33.0),
+            ("1-03", "2-03", 0.0, 0.0, 0.0),
+        ],
+        ["id_df1", "id_df2", "sex_weight", "forename_weight", "match_score"],
+    )
+
+    # Act
+    test_output = msf.assign_match_score(df_with_weights=test_input)
+
+    # Assert
+    ch.assert_df_equality(df1=test_output, df2=expected_output, ignore_row_order=True)
 
 
 def test_assign_weights(spark):
-  """
-  Tests that assign_weights() gives the correct output when provided with
-  suitable inputs.
-  
-  Dependencies: 
-      chispa as ch
-  """
-  # Arrange
-  test_input_df = spark.createDataFrame(
-    [
-      ('1-01', '2-01', 1,    1,    'SARAH',   'SAARAH',  True,  0.8889,
-       False, True,  False, False, True),
-      ('1-02', '2-02', 1,    2,    'ALEECHA', 'ALEESHA', False, 0.6667,
-       True,  False, False, True,  False),
-      ('1-03', '2-03', 2,    1,    'TOM',     'GRACE',   False, 0.0000,
-       True,  False, True,  False, False)
-    ],
-    [
-      'id_df1', 'id_df2', 'sex_df1', 'sex_df2', 'forename_df1', 'forename_df2',
-      'sex_agr_state', 'forename_sorensen_dice', 'di_sex_1', 'di_sex_2',
-      'di_forename_1', 'di_forename_2', 'di_forename_3'
-    ]
-  )
-  
-  test_input_x_star = {
-    'sex_disagree': 0.0,
-    'sex_agree': 75.0,
-    'forename_disagree': 0.0,
-    'forename_partially_agree_1': 33.0,
-    'forename_agree': 91.0,
-  }
-  
-  expected_output = spark.createDataFrame(
-    [
-      ('1-01', '2-01', 75.0,  91.0),
-      ('1-02', '2-02',  0.0,  33.0),
-      ('1-03', '2-03',  0.0,  0.0)
-    ],
-    [
-      'id_df1', 'id_df2', 'sex_weight', 'forename_weight'
-    ]
-  )
+    """
+    Tests that assign_weights() gives the correct output when provided with
+    suitable inputs.
 
-  # Act
-  test_output = msf.assign_weights(
-    df_with_deltas = test_input_df,
-    df1_id = 'id_df1',
-    df2_id = 'id_df2',
-    cutpoints = {
-      'sex': None,
-      'forename': [0.5, 0.8]
-    },
-    x_star_scaled = test_input_x_star,
-    spark = spark
-  )
-  
-  # Assert
-  ch.assert_df_equality(
-    df1 = test_output,
-    df2 = expected_output,
-    ignore_row_order = True
-  )
+    Dependencies:
+        chispa as ch
+    """
+    # Arrange
+    test_input_df = spark.createDataFrame(
+        [
+            (
+                "1-01",
+                "2-01",
+                1,
+                1,
+                "SARAH",
+                "SAARAH",
+                True,
+                0.8889,
+                False,
+                True,
+                False,
+                False,
+                True,
+            ),
+            (
+                "1-02",
+                "2-02",
+                1,
+                2,
+                "ALEECHA",
+                "ALEESHA",
+                False,
+                0.6667,
+                True,
+                False,
+                False,
+                True,
+                False,
+            ),
+            (
+                "1-03",
+                "2-03",
+                2,
+                1,
+                "TOM",
+                "GRACE",
+                False,
+                0.0000,
+                True,
+                False,
+                True,
+                False,
+                False,
+            ),
+        ],
+        [
+            "id_df1",
+            "id_df2",
+            "sex_df1",
+            "sex_df2",
+            "forename_df1",
+            "forename_df2",
+            "sex_agr_state",
+            "forename_sorensen_dice",
+            "di_sex_1",
+            "di_sex_2",
+            "di_forename_1",
+            "di_forename_2",
+            "di_forename_3",
+        ],
+    )
+
+    test_input_x_star = {
+        "sex_disagree": 0.0,
+        "sex_agree": 75.0,
+        "forename_disagree": 0.0,
+        "forename_partially_agree_1": 33.0,
+        "forename_agree": 91.0,
+    }
+
+    expected_output = spark.createDataFrame(
+        [
+            ("1-01", "2-01", 75.0, 91.0),
+            ("1-02", "2-02", 0.0, 33.0),
+            ("1-03", "2-03", 0.0, 0.0),
+        ],
+        ["id_df1", "id_df2", "sex_weight", "forename_weight"],
+    )
+
+    # Act
+    test_output = msf.assign_weights(
+        df_with_deltas=test_input_df,
+        df1_id="id_df1",
+        df2_id="id_df2",
+        cutpoints={"sex": None, "forename": [0.5, 0.8]},
+        x_star_scaled=test_input_x_star,
+        spark=spark,
+    )
+
+    # Assert
+    ch.assert_df_equality(df1=test_output, df2=expected_output, ignore_row_order=True)
 
 
 @pytest.mark.skip(reason="test shell")
